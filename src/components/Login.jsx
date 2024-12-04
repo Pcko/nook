228790323cb15f3a2733181f4aebc93ef80aced3
Florian <210106@studierende.htl-donaustadt.at>
2024-12-04 13:39:48 +0100

    style(tailwind config): switch to using tailwind themes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,18 @@
 import logo from '../assets/resources/Image.png'
 function Login(){
     return (
-        <div className="flex items-center justify-center bg-[#202020] h-full w-full">
-            <div id="Window" className="h-[400px] w-[300px] p-2 text-white bg-[#303030] rounded-xl p-5">
+        <div className="flex items-center justify-center bg-website-bg h-full w-full">
+            <div id="Window" className="h-[400px] w-[300px] p-2 text-white bg-ui-bg rounded-xl p-5">
                 <div className={"flex items-center justify-center mt-5 mb-10"}>
                     <img src={logo} className="w-1/2"/>
                 </div>
                 <h6>Username</h6>
                 <input type="text" id="username"
-                    className="w-full border-white border-[1px] bg-[#202020] pl-1 pr-1 mb-3"></input>
+                    className="w-full border-white border-[1px] bg-ui-bg pl-1 pr-1 mb-3"></input>
                 <h6>Password</h6>
                 <input type="password" id="password"
-                    className="w-full border-white border-[1px] bg-[#202020] pl-1 pr-1"></input>
-                <button id="sign-up" className=" mx-auto mt-5 rounded-xl bg-[#6B439B] font-bold py-2 px-4 rounded-full">Sign up</button>
+                    className="w-full border-white border-[1px] bg-ui-bg pl-1 pr-1"></input>
+                <button id="sign-up" className="hover:bg-primary-hover mx-auto mt-5 rounded-xl bg-primary font-bold py-2 px-4 rounded-full">Sign up</button>
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary":"#6B439B",
+        "secondary":"#8869AD",
+        "primary-hover":"#5A3382",
+        "website-bg":"#202020",
+        "ui-bg":"#303030"
+      },
+      spacing:{
+
+      }
+    },
   },
   plugins: [],
 }
