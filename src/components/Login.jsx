9e936b6fc018fb53ee9a4458d1f2a05ad44760ac
Nicolas Seiser <126863503+Pcko@users.noreply.github.com>
2024-12-04 10:42:49 +0100

    init(login): create branch for login-feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,18 @@
-import logo from '../assets/resources/webcent.png'
+import logo from '../assets/resources/Image.png'
 function Login(){
     return (
         <div className="flex items-center justify-center bg-[#202020] h-full w-full">
-            <div className="h-[300px] w-[300px] p-2 text-white">
-                <div className={"w-full pb-10"}>
-                    <img src={logo}/>
+            <div id="Window" className="h-[400px] w-[300px] p-2 text-white bg-[#303030] rounded-xl p-5">
+                <div className={"flex items-center justify-center mt-5 mb-10"}>
+                    <img src={logo} className="w-1/2"/>
                 </div>
                 <h6>Username</h6>
                 <input type="text" id="username"
-                       className="w-full border-white border-[1px] bg-[#202020] pl-1 pr-1 mb-3"></input>
+                    className="w-full border-white border-[1px] bg-[#202020] pl-1 pr-1 mb-3"></input>
                 <h6>Password</h6>
                 <input type="password" id="password"
-                       className="w-full border-white border-[1px] bg-[#202020] pl-1 pr-1"></input>
-                <button id="sign-up" className="text-[#505050] block mx-auto mt-8">Sign up</button>
+                    className="w-full border-white border-[1px] bg-[#202020] pl-1 pr-1"></input>
+                <button id="sign-up" className=" mx-auto mt-5 rounded-xl bg-[#6B439B] font-bold py-2 px-4 rounded-full">Sign up</button>
             </div>
         </div>
     );
