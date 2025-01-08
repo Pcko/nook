566fa40169caf952e938bdf5ac8cddfc98693101
Moritz Wenzl <210353@studierende.htl-donaustadt.at>
2025-01-08 12:30:13 +0100

    nf(webtokens):add basic functions

--- api/routes/auth-login.js
+++ api/routes/auth-login.js
@@ -1,6 +1,7 @@
 import db from "../database/connection.js";
 import { ObjectId } from "mongodb";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import express from 'express';
 const router = express.Router();
 
@@ -12,12 +13,26 @@ router.post('/login', async (req, res) => {
 
   const match = await bcrypt.compare(data.password, result.password);
 
-  if (match) {
-    res.status(200).send({ message: 'User credentials valid' });
+  if (!match) {
+    res.status(403);
     return;
   }
 
-  res.status(403).send({ message: 'Username or password incorrect' });
+  let userid = result._id;
+
+  console.log('First') //dev
+  console.log(process.env.ACCESS_TOKEN_SECRET) //dev
+  const accessToken = jwt.sign(userid, process.env.ACCESS_TOKEN_SECRET)
+
+  console.log({ accessToken: accessToken })
+
+  console.log('Second')
+  const user = { id: userid }
+  const accessToken2 = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+
+  console.log({ accessToken: accessToken2 })
+
+  //res.status(200).;
 });
 
 export default router;
