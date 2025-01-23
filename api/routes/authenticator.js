4b84c7bcd307cbb54b58ca181848d90f367ee618
Moritz Wenzl <210353@studierende.htl-donaustadt.at>
2025-01-23 13:05:47 +0100

    feature(register): add register functionality
    Register connected with database;
    Small refactor

--- api/routes/authenticator.js
+++ api/routes/authenticator.js
@@ -1,4 +1,4 @@
-//import db from "../database/connection.js";
+import db from "../database/connection.js";
 import { ObjectId } from "mongodb";
 import bcrypt from 'bcrypt';
 import express from 'express';
@@ -7,25 +7,60 @@ const router = express.Router();
 
 // login authenticator request
 router.post('/login', async (req, res) => { 
-  const data = req.body;
+  try{
+    const body = req.body;
 
-  let collection = await db.collection('users');
-  let result = await collection.findOne({ username: data.username });
+    //search for username in db
+    const collection = await db.collection('users');
+    const result = await collection.findOne({ username: body.username });
+  
+    //validate the password
+    const match = await bcrypt.compare(body.password, result.password);
+    if (match) {
+      res.status(200).send({ message: 'User credentials valid' });
+      return;
+    }
 
-  const match = await bcrypt.compare(data.password, result.password);
-
-  if (match) {
-    res.status(200).send({ message: 'User credentials valid' });
-    return;
+    res.status(403).send({ message: 'Username or password incorrect' });
   }
-
-  res.status(403).send({ message: 'Username or password incorrect' });
+  catch(e){
+    return res.sendStatus(500);
+  }
 });
 
 
 // account registration request
 router.post('/register', async (req, res) => {
-  res.sendStatus(501);
+  try{
+    const body = req.body;
+
+    //make sure request body is not invalid
+    if(!body || !body.password || !body.firstName || !body.lastName || !body.email || !body.username){
+      return res.sendStatus(400);
+    }
+  
+    //search for username in db
+    const collection = await db.collection('users');
+    const result = await collection.findOne({ username: body.username });
+    
+    if(result){
+      return res.status(409).send('This username not available');
+    }
+
+    //create new user
+    const newUser = {
+      username: body.username,
+      password: await bcrypt.hash(body.password, 10)
+    }
+
+    //inserting new user into db
+    await collection.insertOne(newUser);
+    
+    res.sendStatus(201);
+  }
+  catch(e){
+    return res.sendStatus(500);
+  }
 });
 
 
