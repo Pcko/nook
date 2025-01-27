a14145bc98d021f5e6e6dd3b14e2f01c24dfa324
Florian Schlögl <145751487+Bohnengulaschhandelsstandbesitzer@users.noreply.github.com>
2025-01-27 07:57:35 +0100

    init(settings): create react component

---
+++ app/src/components/general/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/resources/image.png'
+
+function Settings(){
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Settings;
