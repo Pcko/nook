242029edf8d500ddbcca24550471e916c77cb778
Moritz Wenzl <210353@studierende.htl-donaustadt.at>
2025-01-29 16:42:56 +0100

    add(register): add full user dataset

--- api/database/connection.js
+++ api/database/connection.js
@@ -28,6 +28,6 @@ try {
     console.error(err);
 }
 
-let db = client.db("NookDB");
+let db = client.db("NookTestDB");
 
 export default db;
--- api/routes/authenticator.js
+++ api/routes/authenticator.js
@@ -6,14 +6,14 @@ const router = express.Router();
 
 
 // login authenticator request
-router.post('/login', async (req, res) => { 
-  try{
+router.post('/login', async (req, res) => {
+  try {
     const body = req.body;
 
     //search for username in db
     const collection = await db.collection('users');
     const result = await collection.findOne({ username: body.username });
-  
+
     //validate the password
     const match = await bcrypt.compare(body.password, result.password);
     if (match) {
@@ -23,7 +23,7 @@ router.post('/login', async (req, res) => {
 
     res.status(403).send({ message: 'Username or password incorrect' });
   }
-  catch(e){
+  catch (e) {
     return res.sendStatus(500);
   }
 });
@@ -31,34 +31,38 @@ router.post('/login', async (req, res) => {
 
 // account registration request
 router.post('/register', async (req, res) => {
-  try{
+  try {
     const body = req.body;
 
     //make sure request body is not invalid
-    if(!body || !body.password || !body.firstName || !body.lastName || !body.email || !body.username){
+    if (!body || !body.username || !body.password || !body.firstName || !body.lastName || !body.email) {
       return res.sendStatus(400);
     }
-  
+
     //search for username in db
     const collection = await db.collection('users');
-    const result = await collection.findOne({ username: body.username });
-    
-    if(result){
+    const result = await collection.findOne({ _id: body.username });
+
+    if (result) {
       return res.status(409).send('This username not available');
     }
 
     //create new user
     const newUser = {
+      _id: body.username,
       username: body.username,
-      password: await bcrypt.hash(body.password, 10)
+      password: await bcrypt.hash(body.password, 10),
+      firstName: body.firstName,
+      lastName: body.lastName,
+      email: body.email
     }
 
     //inserting new user into db
     await collection.insertOne(newUser);
-    
+
     res.sendStatus(201);
   }
-  catch(e){
+  catch (e) {
     return res.sendStatus(500);
   }
 });
