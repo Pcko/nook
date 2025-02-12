f62d0cad1fc5a99ab5daa0dc07ef096158fbf6fd
Moritz Wenzl <210353@studierende.htl-donaustadt.at>
2025-02-12 20:18:44 +0100

    fix(account-deletion): correct axios request

--- app/src/components/settings/AccountDeletionConfirmationForm.jsx
+++ app/src/components/settings/AccountDeletionConfirmationForm.jsx
@@ -1,38 +1,38 @@
 import axios from '../auth/AxiosInstance';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-function AccountDeletionConfirmationForm(){
-    const handleAccountDeletion = async ()=>{
-        try{
-            const response = await axios.post('/api/delete-account');
+function AccountDeletionConfirmationForm() {
+    const handleAccountDeletion = async () => {
+        try {
+            const response = await axios.delete('/api/settings/delete-account', JSON.parse(localStorage.getItem('user')).username);
 
             localStorage.removeItem('accessToken');
             localStorage.removeItem('refreshToken');
             useNavigate()('/login');
         }
-        catch(e){
+        catch (e) {
             alert('There was an error trying to delete your account. Please contact our support team for help.')
         }
     };
 
-    return(
-      <div className="border-[1px] border-ui-border bg-ui-bg w-[500px] p-8 rounded-lg">
-          <h1 className="text-3xl mb-2">We're sorry to see you go</h1>
-          <p className="mb-4">This deletion is final and there is <span className="text-dangerous">no way to recover your account</span> or any data if you choose to continue. Do you want to delete your account?</p>
-          <div className="flex">
-              <input
-                  type="button"
-                  value="Cancel"
-                  className="btn hover:bg-ui-button-hover bg-ui-button border-ui-border border-[1px] w-[35%] ml-[25%]"
-              />
-              <input
-                  type="button"
-                  value="Delete"
-                  className="btn bg-dangerous hover:bg-dangerous hover:animate-pulse w-[35%] mr-0"
-                  onClick={handleAccountDeletion}
-              />
-          </div>
-      </div>
+    return (
+        <div className="border-[1px] border-ui-border bg-ui-bg w-[500px] p-8 rounded-lg">
+            <h1 className="text-3xl mb-2">We're sorry to see you go</h1>
+            <p className="mb-4">This deletion is final and there is <span className="text-dangerous">no way to recover your account</span> or any data if you choose to continue. Do you want to delete your account?</p>
+            <div className="flex">
+                <input
+                    type="button"
+                    value="Cancel"
+                    className="btn hover:bg-ui-button-hover bg-ui-button border-ui-border border-[1px] w-[35%] ml-[25%]"
+                />
+                <input
+                    type="button"
+                    value="Delete"
+                    className="btn bg-dangerous hover:bg-dangerous hover:animate-pulse w-[35%] mr-0"
+                    onClick={handleAccountDeletion}
+                />
+            </div>
+        </div>
     );
 }
 
