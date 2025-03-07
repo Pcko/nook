90d1556f3ae7e592e94c698ed77a90fb08009144
Moritz Wenzl <210353@studierende.htl-donaustadt.at>
2025-03-07 13:03:02 +0100

    fix(axiosInterceptor): use the configured axios-instance instead of default axios

--- app/src/components/auth/AxiosInstance.js
+++ app/src/components/auth/AxiosInstance.js
@@ -35,7 +35,7 @@ axiosInstance.interceptors.response.use(
                     return Promise.reject(error);
                 }
 
-                const response = await axios.post('/auth/token', { 'token': refreshToken });
+                const response = await axiosInstance.post('/auth/token', { 'token': refreshToken });
                 const { accessToken } = response.data;
 
                 localStorage.setItem('accessToken', accessToken);
