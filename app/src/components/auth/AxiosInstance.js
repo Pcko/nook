58c7727a024eb19c48bf3b049366e5eca4026c25
Florian Schlögl <145751487+Bohnengulaschhandelsstandbesitzer@users.noreply.github.com>
2025-04-23 20:00:37 +0200

    fix(axios-instance): add missing brackets to function call

--- app/src/components/auth/AxiosInstance.js
+++ app/src/components/auth/AxiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useNotifications } from "../general/NotificationContext";
-const { showNotification } = useNotifications;
+const { showNotification } = useNotifications();
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
