9762ebe5de8bf34a3158c657fe5e2080aab401f9
Florian Schlögl <145751487+Bohnengulaschhandelsstandbesitzer@users.noreply.github.com>
2025-04-24 23:10:31 +0200

    fix(themes): fix dark system theme initialization

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -16,15 +16,23 @@ import NotificationOverlay from "./components/general/NotificationOverlay";
 
 function App() {
     useEffect(()=>{
-        const theme = localStorage.getItem('theme') || 'system';
         const accessibilityMode = localStorage.getItem('accessibility');
+        let theme = localStorage.getItem('theme');
+        if (!theme) {
+            theme = 'system';
+            localStorage.setItem('theme', theme);
+        }
+
+        if (accessibilityMode === 'high-contrast'){
+            document.documentElement.classList.add('high-contrast');
+        }
 
         if (theme === 'system') {
             if (window.matchMedia('(prefers-color-scheme: light)').matches) {
                 document.documentElement.classList.add('light');
+            } else {
+                document.documentElement.classList.add('dark');
             }
-        } else if (accessibilityMode === 'high-contrast'){
-            document.documentElement.classList.add('high-contrast');
         } else {
             document.documentElement.classList.add(theme);
         }
