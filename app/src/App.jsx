08908a662ab0d392da3bce62a3dadc7b2e1de32e
Nicolas-Seiser <126863503+Pcko@users.noreply.github.com>
2025-09-17 19:58:23 +0200

    fix(router): revert accidental code removal

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -42,7 +42,21 @@ function App() {
         <NotificationProvider>
             <NotificationOverlay />
             <Router>
-                <EditorHub/>
+                <div className={'h-full'}>
+                    <main className={'h-full bg-far-bg text-text'}>
+                        <Routes>
+                            <Route path="/" element={<AuthRedirect/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                            <Route path="/register" element={<Register/>}/>
+
+                            <Route element={<ProtectedRoute/>}>
+                                <Route path="/settings" element={<Settings/>}/>
+                                <Route path="/dashboard" element={<Dashboard/>}/>
+                                <Route path="/editor/:projectName/:pageName" element={<EditorHub/>}/>
+                            </Route>
+                        </Routes>
+                    </main>
+                </div>
             </Router>
         </NotificationProvider>
     );
