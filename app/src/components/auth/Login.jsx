b07fbbe09e650afbffdd74b81da90445d2e20016
Nicolas-Seiser <126863503+Pcko@users.noreply.github.com>
2025-09-26 23:02:42 +0200

    refactor(login): make login-v2 prototype
    - temporarily changed light theme
    - changed font of app

--- app/src/components/auth/Login.jsx
+++ app/src/components/auth/Login.jsx
@@ -1,21 +1,21 @@
 import {useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import axios from '../auth/AxiosInstance'
-import ImageCarousel from './ImageCarousel';
-import { useNotifications } from '../general/NotificationContext';
-import { isInvalidStringForUsername, isInvalidStringForPassword } from '../general/FormChecks';
-import CenteredWindowWithBackgroundBlur from '../general/CenteredWindowWithBackgroundBlur';
+import {useNotifications} from '../general/NotificationContext';
+import {isInvalidStringForPassword, isInvalidStringForUsername} from '../general/FormChecks';
 import TwoFactorAuthenticationCodeInputForm from './TwoFactorAuthenticationCodeInputForm';
-import NookBackground from "../general/NookBackground";
 import LoadingScreen from '../general/LoadingScreen';
 import useErrorHandler from "../general/ErrorHandler";
+import Divider from "./FormDivider";
+import { FcGoogle } from "react-icons/fc";
+import AuthScreenDesktopIcon from "./AuthScreenDesktopIcon";
 
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
-    const { showNotification } = useNotifications();
+    const {showNotification} = useNotifications();
     const [twoFactorAuthenticationFormActive, setTwoFactorAuthenticationFormActive] = useState(false);
     const handleError = useErrorHandler();
 
@@ -75,81 +75,86 @@ function Login() {
         setLoading(true);
 
         try {
-            const response = await axios.post('/auth/login', { username, password, otp: twoFactorAuthenticationCode });
+            const response = await axios.post('/auth/login', {username, password, otp: twoFactorAuthenticationCode});
             setTwoFactorAuthenticationFormActive(false);
             closeLogin(response.data.accessToken, response.data.refreshToken, response.data.user);
-        }
-        catch (err) {
+        } catch (err) {
             handleError(err);
         }
 
         setLoading(false);
     };
 
-    if(loading){
+    if (loading) {
         return <LoadingScreen/>
     }
 
     return (
         <div className="flex items-center justify-center bg-website-bg h-full w-full">
-            <NookBackground/>
-            <div id="Window"
-                 className="w-[1000px] text-text bg-ui-bg border-[1px] border-ui-border rounded-[10px] z-10">
-                <div className="w-fit h-fit grid grid-cols-2 gap-[2vw] m-3">
-                    <ImageCarousel/>
-                    <div className="mx-[14%] my-[10%]">
-                        <h1 className="text-4xl mb-3">Login</h1>
+            <div id="Window" className="flex w-[98%] h-[98%] text-text bg-ui-bg border-[1px] border-ui-border rounded-[10px] z-10">
+                <div className="bg-blue w-[45%] p-aut flex-none justify-items-center self-center">
+                    <h1 className={"font-semibold text-4xl"}>Welcome Back</h1>
+                    <p>Don’t have an Account? <a className={"text-ui-subtle hover:cursor-pointer"} onClick={() => navigate('/login')}>Register Now.</a></p>
+                    <form onSubmit={handleSubmit} className={"w-[45%]"}>
+                        {/* Username Field */}
+                        <label htmlFor="username" className="block mb-1 mt-6">Username</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            autoComplete={"username"}
+                            required
+                            minLength="2"
+                            className="form-field mb-3"
+                            onChange={(e) => setUsername(e.target.value)}
+                            value={username}
+                        />
 
-                        <span>Don't have an account yet? </span>
-                        <a className={"text-ui-subtle underline hover:cursor-pointer"}
-                            onClick={() => navigate('/register')}>Sign up</a>
+                        {/* Password Field */}
+                        <label htmlFor="password" className="block">Password
+                            <a className={"text-ui-subtle hover:cursor-pointer ml-3"}
+                               onClick={() => navigate('/register')}>Forgot your password?</a>
+                        </label>
 
-                        <form onSubmit={handleSubmit}>
-                            {/* Username Field */}
-                            <label htmlFor="username" className="block mb-1 mt-6">Username</label>
-                            <input
-                                type="text"
-                                id="username"
-                                name="username"
-                                required
-                                minLength="2"
-                                className="w-full h-8 px-2 border-ui-border focus:border-ui-border-selected focus:outline-none border-[1px] rounded bg-ui-bg mb-3"
-                                onChange={(e) => setUsername(e.target.value)}
-                                value={username}
-                            />
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            autoComplete={"current-password"}
+                            required
+                            minLength="10"
+                            className="form-field"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                        />
 
-                            {/* Password Field */}
-                            <label htmlFor="password" className="block">Password</label>
-                            <input
-                                type="password"
-                                id="password"
-                                name="password"
-                                required
-                                minLength="10"
-                                className="w-full h-8 px-2 border-ui-border focus:border-ui-border-selected focus:outline-none border-[1px] rounded bg-ui-bg"
-                                onChange={(e) => setPassword(e.target.value)}
-                                value={password}
-                            />
+                        {/* Sign-in Button */}
+                        <input
+                            type={"submit"}
+                            id="sign-up"
+                            className={`btn w-full mt-10 ${loading ? 'animate-pulse' : ''}`}
+                            value="Login"
+                        >
+                        </input>
 
-                            <a className={"text-ui-subtle text-xs underline hover:cursor-pointer"}
-                                onClick={() => navigate('/register')}>Forgot your password?</a>
+                        <Divider className={"mt-7"} dividerText={"Or Login With"}/>
 
-                            {/* Sign-in Button */}
-                            <input
-                                type={"submit"}
-                                id="sign-up"
-                                className={`btn w-full mt-10 ${loading ? 'animate-pulse' : ''}`}
-                                value="Sign in"
-                            >
-                            </input>
-                        </form>
-                    </div>
+                        <button
+                            className={`btn border-ui-border bg-white w-full mt-10 flex items-center justify-center gap-2 hover:bg-ui-button-hover ${loading ? 'animate-pulse' : ''}`}>
+                            <FcGoogle className="text-xl"/>
+                            <span className={"text-text"}>Google</span>
+                        </button>
+
+                    </form>
+                </div>
+                <div className="flex-1 justify-items-center self-center">
+                    <AuthScreenDesktopIcon/>
                 </div>
             </div>
 
             {/* Dynamically rendered form */}
             {twoFactorAuthenticationFormActive ?
-                <TwoFactorAuthenticationCodeInputForm submitForm={handle2FASubmit} />
+                <TwoFactorAuthenticationCodeInputForm submitForm={handle2FASubmit}/>
                 : ''}
         </div>
     );
