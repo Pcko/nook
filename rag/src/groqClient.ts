8270c2da824ce6810a4860a2b96c9fa6d3c7b27c
Florian Schlögl <145751487+Bohnengulaschhandelsstandbesitzer@users.noreply.github.com>
2025-10-10 13:00:01 +0200

    feature(rag-generation): add groq and streaming option

---
+++ rag/src/groqClient.ts
@@ -0,0 +1,64 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+import type {QueryResponseBody} from "./dto/queryResponseBody.dto.js";
+import Groq from 'groq-sdk';
+import {type StreamCallback} from './types/StreamCallback.js';
+
+const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY,
+});
+
+async function streamGroqResponse(query: string, onData: StreamCallback) {
+    try {
+        const stream = await groq.chat.completions.create({
+            messages: [{ role: 'user', content: query }],
+            model: process.env.GROQ_LLM_MODEL || 'qwen/qwen3-32b',
+            stream: true,
+        });
+
+        for await (const chunk of stream) {
+            const content = chunk.choices[0]?.delta?.content;
+            if (content) {
+                onData(content);
+            }
+        }
+    } catch (err) {
+        console.error('Streaming error from Groq:', err);
+        throw err;
+    }
+}
+
+async function getGroqResponse(query: string): Promise<QueryResponseBody> {
+    let data = '';
+    try{
+        const startingTime = process.hrtime();
+        const response = await groq.chat.completions.create({
+            messages: [{ role: 'user', content: query }],
+            model: process.env.GROQ_LLM_MODEL || 'qwen/qwen3-32b',
+            stream: false,
+        });
+
+        const data = response.choices[0]?.message?.content || '';
+
+        const duration = process.hrtime(startingTime);
+
+        const thinkMatch = data.match(/<think>(.*?)<\/think>/s);
+        const think = (thinkMatch ? thinkMatch[1]?.trim() : '') || '';
+        const trimmedResponse = data.replace(/<think>.*?<\/think>/s, '').trim();
+
+        return {
+            think: think,
+            response: trimmedResponse,
+            total_duration: duration[1]
+        }
+    } catch (err) {
+        console.error('Response Error from Groq: ', err);
+        throw err;
+    }
+}
+
+export default {
+    streamGroqResponse,
+    getGroqResponse
+};
--- rag/src/routers/ragRouter.ts
+++ rag/src/routers/ragRouter.ts
@@ -1,33 +1,49 @@
 import {type Request, type Response, Router} from "express";
 import { type QueryRequestBody} from "../dto/queryRequestBody.dto.js";
 import chromaClient from "../chromadbClient.js";
-import type {LlmResponseBody} from "../dto/llmResponseBody.dto.js";
-import {getLLMResponse} from "../localLLMClient.js";
+import localLLMClient from "../localLLMClient.js";
 import type {QueryResponseBody} from "../dto/queryResponseBody.dto.js";
+import groqClient from "../groqClient.js";
 
 const ragRouter = Router();
 
 ragRouter.post('/query', async (req: Request<{}, {}, QueryRequestBody>, res: Response<QueryResponseBody| { error: string }>) => {
     const queryRequest = req.body;
 
-    // Send Request to ChromaDB if queryRequest.skipContext isn't true
     let query = queryRequest.query;
     if(!queryRequest.skipContext) {
         const chromaResponse = await chromaClient.query(query);
         query = query+`\nContext: ${chromaResponse.documents}`;
     }
 
-    // Get Response from LLM
-    let llmResponse: LlmResponseBody;
-    if(queryRequest.useLocalLLM) {
-        llmResponse = await getLLMResponse(query);
-    } else {
-        return res.sendStatus(501);
-    }
+    console.log(queryRequest);
+    if(queryRequest.stream) {
+        res.setHeader('Content-Type', 'text/event-stream');
+        res.setHeader('Cache-Control', 'no-cache');
+        res.setHeader('Connection', 'keep-alive');
 
-    // Return LLM Response
-    const queryResponseBody: QueryResponseBody = llmResponse;
-    res.status(200).send(queryResponseBody);
+        const callback = (chunk: string) => {
+            res.write(chunk);
+        }
+
+        if(queryRequest.useLocalLLM) {
+            await localLLMClient.streamLLMResponse(query, callback);
+        } else {
+            await groqClient.streamGroqResponse(query, callback);
+        }
+
+        return res.end();
+    } else {
+        let queryResponse: QueryResponseBody;
+
+        if(queryRequest.useLocalLLM) {
+            queryResponse = await localLLMClient.getLLMResponse(query);
+        } else {
+            queryResponse = await groqClient.getGroqResponse(query);
+        }
+
+        return res.status(200).send(queryResponse);
+    }
 });
 
 export default ragRouter;
