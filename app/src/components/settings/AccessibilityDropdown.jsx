eaf3d64dd15a88d5d22e5fdf7177d59c913dc7c2
Nicolas-Seiser <126863503+Pcko@users.noreply.github.com>
2025-11-05 10:49:18 +0100

    fix(style): change accessibility theme slightly to conform to standards
    - change attributes that are static to dynamic values

--- app/src/components/settings/AccessibilityDropdown.jsx
+++ app/src/components/settings/AccessibilityDropdown.jsx
@@ -11,7 +11,7 @@ function AccessibilityDropdown({ options, selected, onChange }) {
         <div className="w-1/2 ml-auto mr-0 relative">
             <Listbox value={selected} onChange={onChange}>
                 <div className="relative">
-                    <Listbox.Button className="w-full py-1 rounded-[5px] text-text text-center bg-ui-default border-2 border-ui-border cursor-pointer">
+                    <Listbox.Button className="w-full py-1 rounded-[5px] text-text text-center bg-ui-bg border-2 border-ui-border cursor-pointer">
                         {optionLabels[selected]}
                     </Listbox.Button>
 
