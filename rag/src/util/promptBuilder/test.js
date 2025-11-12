406af63ac709350c655ec29cecaff67b3759b50b
Todorovitsch Julian <210241@studierende.htl-donaustadt.at>
2025-11-12 13:58:32 +0100

    refactor(prompt-builder): remove unused test file

--- rag/src/util/promptBuilder/test.js
+++
@@ -1,3 +0,0 @@
-import {promptBuilder} from "../../../dist/util/promptBuilder/promptBuilder.js";
-const result =await promptBuilder.build("i am a plumber and i need a website!");
-console.log(result)
